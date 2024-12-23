9ee5a613464ce260a686b742ecab8bb1851f1c07
Tigran Elyazyan <elyazyan.t@gmail.com>
2024-12-23 17:32:01 +0400

    add new color constants for primary, green, and blue

--- lib/presentation/core/consts/colors.dart
+++ lib/presentation/core/consts/colors.dart
@@ -3,7 +3,13 @@ import 'dart:ui';
 class AppColors {
   const AppColors._();
 
+  static const primary = Color(0xffff575c);
+
   static const error = Color(0xffFF7F7F);
 
   static const success = Color(0xff66BB6A);
+
+  static const green = Color(0xFF4aaf57);
+
+  static const blue = Color(0xFF4a90e2);
 }
